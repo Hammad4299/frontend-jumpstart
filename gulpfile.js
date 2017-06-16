56e759a074fa2a3476972db72b1d0e7d9a273c6c
Hammad <hammad4299@gmail>
2017-06-16 21:16:54 +0500

    EJS Gulp added

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,16 @@ var gulp = require('gulp');
 var swig = require('gulp-swig');
 var data = require('gulp-data');
 var watch = require('gulp-watch');
+var ejs = require('gulp-ejs');
+var gutil = require('gulp-util');
 
 let paths = {
     src: path.join(__dirname,'src'),
     output: path.join(__dirname,'dist')
 };
 
-gulp.task('compile-templates', function() {
-    let viewPath = path.join(paths.src,'views/pages/**/*');
+gulp.task('compile-templates-swig', function() {
+    let viewPath = path.join(paths.src,'views/pages/swig/**/*');
     return watch(viewPath,function () {
         gulp.src(viewPath)
             .pipe(data(function (file) {
@@ -20,6 +22,19 @@ gulp.task('compile-templates', function() {
                 };
             }))
             .pipe(swig())
+            .pipe(gulp.dest(path.join(paths.output)))
+    });
+});
+
+gulp.task('compile-templates-ejs', function() {
+    let viewPath = path.join(paths.src,'views/pages/ejs/**/*');
+    return watch(viewPath,function () {
+        gulp.src(viewPath)
+            .pipe(ejs({
+                data: 'ABC'
+            },{},{
+                ext: '.html'
+            })).on('error', gutil.log)
             .pipe(gulp.dest(path.join(paths.output)))
     });
 });

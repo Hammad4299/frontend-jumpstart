2f985cdda90173f053f2b628d2cadd7bec91b473
Hammad <hammad4299@gmail>
2020-07-30 00:05:54 +0500

    number input

--- src/js/react/hooks/index.ts
+++ src/js/react/hooks/index.ts
@@ -1,2 +1,45 @@
 export * from "./breadcrumb";
 export * from "./routing";
+import { useState, useEffect } from "react";
+import numeral from "numeral";
+
+export function isNumber(value: any) {
+    return numeral.validate(value, numeral.locale());
+}
+
+export function useNumberInput(
+    onChangeValue: (value: number) => void,
+    defaultValue = 0,
+    options: {
+        format: string;
+    } = { format: "0,0.00" }
+) {
+    const [textState, setTextState] = useState(
+        defaultValue === null
+            ? ""
+            : numeral(defaultValue).format(options.format)
+    );
+
+    const toNum = (value: string) => {
+        return isNumber(value) ? numeral(value).value() : null;
+    };
+
+    const onInputChange = (value: string) => {
+        if (value == null) {
+            value = "";
+        }
+        value = value.trim();
+        setTextState(value);
+        return toNum(value);
+    };
+
+    useEffect(() => {
+        onChangeValue(toNum(textState));
+    }, [textState]);
+
+    return {
+        inputValue: textState,
+        onInputChange,
+        value: toNum(textState)
+    };
+}
